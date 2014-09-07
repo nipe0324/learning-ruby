99b2e9869403e9a049303f5468adb2382f2c1046
yanagi0324 <nipe0324@gmail.com>
2014-09-07 17:46:39 +0900

    refactor order.rb

--- pdf_order_sample/app/models/order.rb
+++ pdf_order_sample/app/models/order.rb
@@ -2,8 +2,6 @@ class Order < ActiveRecord::Base
   has_many :line_items
 
   def total_price
-    sum = 0
-    line_items.map { |line_itme| sum += line_itme.total_price }
-    return sum
+    line_items.to_a.sum { |line_itme| line_itme.total_price }
   end
 end

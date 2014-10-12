53e23672edd95f6c12e24c06665f01df13e615ea
nipe0324 <nipe0324@gmail.com>
2014-10-13 01:54:51 +0900

    Use json_response in place of JSON.parse

--- angularjs_todo/spec/controllers/api/task_lists_controller_spec.rb
+++ angularjs_todo/spec/controllers/api/task_lists_controller_spec.rb
@@ -10,16 +10,14 @@ RSpec.describe Api::TaskListsController, :type => :controller do
 
         it "is expected to return json of array of those task lists" do
           get :index
-          tasks = JSON.parse(response.body)
-          expect(tasks).to eq [{ 'id' => user.task_list.id }]
+          expect(json_response).to eq [{ 'id' => user.task_list.id }]
         end
       end
 
       it "is expected to return error json with 401 HTTP status when not authenticated" do
         get :index, format: :json
-        tasks = JSON.parse(response.body)
 
-        expect(tasks['error']).to  eq 'You need to sign in or sign up before continuing.'
+        expect(json_response['error']).to  eq 'You need to sign in or sign up before continuing.'
         expect(response.status).to eq 401
       end
 

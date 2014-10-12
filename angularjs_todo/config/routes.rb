bbdc1378339c6c32e2a8693ff867ba779897cf2b
nipe0324 <nipe0324@gmail.com>
2014-10-13 02:33:07 +0900

    Move angular templates to the separate files

--- angularjs_todo/config/routes.rb
+++ angularjs_todo/config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
 
   get '/dashboard'      => 'templates#index'
   get '/task_lists/:id' => 'templates#index'
+
+  get '/templates/:path.html' => 'templates#template', constraints: { path: /.+/ }
 end

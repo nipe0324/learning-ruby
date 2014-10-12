753358ede39286290d0b91e57f1a36f9c897d30f
nipe0324 <nipe0324@gmail.com>
2014-10-12 23:01:57 +0900

    Stop other users from seeing other user's lists

--- angularjs_todo/spec/controllers/task_lists_controller_spec.rb
+++ angularjs_todo/spec/controllers/task_lists_controller_spec.rb
@@ -1,18 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe TaskListsController, :type => :controller do
-  describe "#show" do
-    it "is expected to return 200 for existing task list" do
-      task_list = create(:task_list)
-      get :show, id: task_list.id
+  context "for a logged-in user with a task list" do
+    let(:task_list) { create(:task_list) }
+    let(:user)      { task_list.owner    }
+    before          { sign_in(user)      }
 
-      expect(response).to be_ok
+    describe "#show" do
+      it "is expected to return 200 for existing task list" do
+        get :show, id: task_list.id
+        expect(response).to be_ok
+      end
+
+      it "is expected to return RecordNotFound when task list doesn't exist" do
+        expect {
+          get :show, id: 0
+        }.to raise_error ActiveRecord::RecordNotFound
+      end
+
+      it "is expected to return 401 Unauthorized when task list doesn't exist" do
+        other_task_list = create(:task_list)
+        get :show, id: other_task_list.id
+        expect(response.status).to eq 401
+      end
     end
 
-    it "is expected to return 404 when task list doesn't exist" do
-      expect {
-        get :show, id: 1
-      }.to raise_error ActiveRecord::RecordNotFound
-    end
   end
 end

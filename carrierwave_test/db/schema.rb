a2281854107a3880a94457e71a9e7a93f16f13cc
nipe0324 <nipe0324@gmail.com>
2014-10-15 00:23:05 +0900

    add crud of carrirwave's image

--- carrierwave_test/db/schema.rb
+++ carrierwave_test/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014140237) do
+ActiveRecord::Schema.define(version: 20141014141147) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end

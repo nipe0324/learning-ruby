6c0f007db88de08ff70729f3c2d513684c724f71
ShojiroYanagisawa <s.yanagisawa@hachimenroppi.com>
2014-11-15 15:42:43 +0900

    Delete csv_export_import_test project

--- csv_export_import_test/app/models/manufacture.rb
+++
@@ -1,3 +0,0 @@
-class Manufacture < ActiveRecord::Base
-  has_many :products
-end
--- csv_export_import_test/db/migrate/20141025104837_create_manufactures.rb
+++
@@ -1,9 +0,0 @@
-class CreateManufactures < ActiveRecord::Migration
-  def change
-    create_table :manufactures do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end

c2221b260d0c7b4f6282b80eacbda0cd9486be80
nipe <nipe0324@gmail.com>
2015-11-20 10:56:29 +0900

    [react_test] finish to implement

--- react_test/config/routes.rb
+++ react_test/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :messages, only: [:index, :create], format: 'json'
+  root 'top#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
